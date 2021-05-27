9883d2e1a624389e93e6b0ec7e40b535487c6d4c
Brais P. Castro <braispcastro@gmail.com>
2021-05-27 15:49:17 +0200

    Comando para obtener la versión

---
+++ BotSRT/Discord/Commands/Version.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace BotSRT.Discord.Commands
+{
+    public class Version : BaseCommandModule
+    {
+        [Command("version")]
+        public async Task VersionCommand(CommandContext context)
+        {
+            await context.Channel.SendMessageAsync(Environment.GetEnvironmentVariable("Version"));
+        }
+    }
+}
